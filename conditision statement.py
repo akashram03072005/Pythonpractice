5ba1f19e3608886a42671c44a390906385a529ce
akashram03072005 <130348634+akashram03072005@users.noreply.github.com>
2023-05-11 13:16:56 +0530

    Add files via upload

---
+++ conditision statement.py
@@ -0,0 +1,24 @@
+    #age=int(input("enter the age"))
+    #if(age>=18):
+        #print("your are a senior citizen")
+    #else:
+        #print("your are not a senior citizen")
+#a=int(input("enter the value of a"))
+#if(a%2==0):
+    #print("a is even")
+#else:
+    #print("a is odd")
+#a=int(input("enter teh value of a"))
+#if(a<0):
+    #print("a is negative")
+#else:
+    #print("a is positive")
+a=int(input("enter the value of a"))
+if(a%3==0):
+      print("a is divisble by 3")
+else:
+    print("d is not divisble by 3")
+
+
+        
+        
