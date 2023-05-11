60643988a9fa06df1add8a998b540ecfee4978b1
akashram03072005 <130348634+akashram03072005@users.noreply.github.com>
2023-05-11 13:14:40 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,17 @@
+
+name=str(input("enter your name:"))
+age=int(input("enter your AGE:"))
+email=str(input("enter your EMAIL:"))
+mobilenumber=str(input("enter your MOBILENUMBER:"))
+address=str(input("enter your ADDRESS:"))
+
+print("---------------------------------")
+print("\tSTUDENT INFORMATION")
+print("---------------------------------")
+print("\tName:",name)
+print("\tAge:",age)
+print("\tEMAIL:",email)
+print("\tMobilenumber:",mobilenumber)
+print("\tAddress:",address)
+print("----------------------------------")
+
