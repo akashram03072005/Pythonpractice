adab89b190971af0134b27251ccbc6af4cbf5ad2
akashram03072005 <130348634+akashram03072005@users.noreply.github.com>
2023-05-11 13:15:05 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,86 @@
+#Relatonal operator
+#greator than
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(a>b)
+
+#lesser than
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(a<b)
+
+#greater than or equal to
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(a>=b)
+
+#lesser tahn or equal to
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(a<=b)
+
+#equal to
+a=int(input("enter the value of a"))
+b=int(input("enter the value of a"))
+print(a==b)
+
+#not equal to
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(a!=b)
+
+#logical operator(and)
+a=int(input("enter the value of a"))
+b=int(input("enter the value of a"))
+c=int(input("enter the value of c"))
+print((a>=b) and (b<=c))
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+print((a!=b) and (b==c))
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+print((a<b) and (b>c))
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+print((a<=b) and (b>=c))
+
+#or
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+print((a>b) or (b<c))
+
+a=int(input("enter the value of a"))
+b=int(input("enter teh value of b"))
+c=int(input("enter the value of c"))
+print((a>b) or (b<=c))
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+print((a>=b) or (b<=c))
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+c=int(input("enter the value of c"))
+print((a==b) or (b==c))
+
+#not
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(a>b)
+
+a=int(input("enter the value of a"))
+b=int(input("enter the value of b"))
+print(not(a>b))
+
+
+
+
+      
